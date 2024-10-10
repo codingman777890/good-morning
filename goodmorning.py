b6a6ff74ca25715d6d3b813c7410f782a2195046
Codingman <freelanceforallatcheap@gmail.com>
2024-10-10 01:32:56 -0700

    Create goodmorning.py

---
+++ goodmorning.py
@@ -0,0 +1,12 @@
+import time
+print("Current Time is",time.strftime("%H:%M"))
+h=int(time.strftime("%H"))
+m=int(time.strftime("%M"))
+if(h<=11 and m<=59):
+    print("Good Morning Sir")
+elif(h>=12 and m<=59 and h<17):
+    print("Good AfterNoon Sir")
+elif(h>=17 and m<=59 and h<21):
+    print("Good Evening Sir")
+else:
+    print("Good Night Sir")
